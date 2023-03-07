e240ad77c19054926c0f86ef52b0ffccb58aa330
evgsh01 <allorean@gmail.com>
2023-03-07 15:18:31 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import logoPngSrc from './images/ReactLogo.png'
+import {ReactComponent as Logo} from './images/logo.svg'
+/* import App from './App'; */
+import './style.css';
+
+
+
+const AppList = () => {
+  const item = ['Мой первый компонент', 'Мой второй компонент'];
+  return (
+    <ul>
+      {item.map((item, index) => {
+        return <li key={index}>{item}</li>
+      })}
+    </ul>
+  );
+}
+
+const AppInput = ({placeholder, label, type}) => {
+  return (
+    <label className='label'>
+      {label}
+      <input placeholder={placeholder} type={type}/>
+    </label>
+    
+  );
+}
+
+const AppHeader = () => {
+  return (
+    <div>
+      <Logo/>
+      <h1 className='header-title'><span>Hello, World!</span></h1>
+    </div>
+  );
+}
+
+const App = () => {
+  return (
+    <>
+      <AppHeader/>
+      <AppList/>
+      <AppInput placeholder='Введите ваше имя' label="Имя" type='text'/>
+      <AppInput placeholder='Введите пароль' label="Пароль" type='password'/>
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App/>);
