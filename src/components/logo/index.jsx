9ed0ecf24abb80b3dd06269e6ecbcb07485c61ef
evgsh01 <allorean@gmail.com>
2023-03-15 21:16:17 +0300

    add logo small

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -1,10 +1,12 @@
 import './styles.css';
 import logoSrc from "./assets/logo.svg";
+import logoSrcSmall from "./assets/logo_small.svg";
 
 export function Logo() {
   return (
     <a href="/" className="logo">
-      <img src={logoSrc} alt='Логотип компании' className="logo__pic"/>
+      <img src={logoSrc} alt='Логотип компании' className="logo__pic" />
+      <img src={logoSrcSmall} alt='Логотип компании' className="logo__pic logo__pic-small" />
     </a>
   );
 }
--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,14 +1,22 @@
 import likeIcon from '../../images/save.svg';
 import dog from './assets/dog.svg';
 import bag from './assets/bag.svg';
+import menu from './assets/menu.svg';
 import './styles.css';
 
 export function Nav() {
     return (
         <nav>
-            <a href="#" className="" title="Избранное"><img src={likeIcon} alt="Сердечко лайка" className="nav__btn-icon" /></a>
-            <a href="#" className="" title="Корзина"><img src={bag} alt="Корзина" className="nav__btn-icon" /></a>
-            <a href="#" className="" title="Личный кабинет"><img src={dog} alt="Мордочка собаки" className="nav__btn-icon" /></a>           
-        </nav>        
+            <ul className='nav__wrapper'>
+                <li className=""><a href="#" className="" title="Избранное"><img src={likeIcon} alt="Избранное" className="nav__btn-icon" /></a></li>
+                <li className=""><a href="#" className="" title="Корзина"><img src={bag} alt="Корзина" className="nav__btn-icon" /></a></li>
+                <li className=""><a href="#" className="" title="Личный кабинет"><img src={dog} alt="Личный кабинет" className="nav__btn-icon" /></a></li>  
+            </ul>
+            <ul className='nav__wrapper-small'>
+                <li className="">
+                    <a href="#" className=""><img src={menu} alt="Меню" className="nav__btn-icon" /></a>
+                </li>
+            </ul>                  
+        </nav> 
     );
 }
