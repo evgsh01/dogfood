fee063d524f61fb7dbba9c502a5d666df488ec62
evgsh01 <allorean@gmail.com>
2023-05-09 23:58:46 +0300

    edit nav

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -3,7 +3,7 @@ import { useContext } from 'react';
 
 import { ReactComponent as FavoriteIcon } from './img/favorites.svg';
 import { ReactComponent as DogIcon } from './img/dog.svg';
-import bag from './img/bag.svg';
+import { ReactComponent as Bag } from './img/bag.svg';
 import menu from './img/menu.svg';
 import { UserContext } from '../../contexts/current-user-context';
 import { CardsContext } from '../../contexts/card-context';
@@ -23,7 +23,9 @@ export function Nav() {
                     {favorites.length !== 0 && <span className={s.iconBubble}>{favorites.length}</span>}
                 </Link>
                 
-                <img src={bag} alt="Корзина" />
+                <Link className={s.favoritesLink} to={{pathname: '/cart'}} state={{ backgroundLocation: location, initialPath: location.pathname }}>
+                    <Bag title='Корзина'/>
+                </Link>                
 
                 <Link className={s.favoritesLink} to={{pathname: '/login'}} state={{ backgroundLocation: location, initialPath: location.pathname }}>
                     <DogIcon title='Личный кабинет'/>
